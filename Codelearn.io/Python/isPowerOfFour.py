d89b3c171b0b70721151ab3e6a62e9cbee6951ef
T3l3sc0p3 <96990857+T3l3sc0p3@users.noreply.github.com>
2023-07-29 16:42:10 +0700

    Initial commit

--- Codelearn.io/Python/isPowerOfFour.py
+++
@@ -1,6 +0,0 @@
-from math import log
-# Check if number is a power of 4
-def isPowerOfFour(n):
-    # Return False if n is not positive
-    if n <= 0: return False
-    return log(n,4).is_integer()
