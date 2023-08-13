c9c5a08504061db3e3e5ca450f90632001d7e5d2
T3l3sc0p3 <96990857+T3l3sc0p3@users.noreply.github.com>
2023-08-14 01:49:53 +0700

    [hackerrank.com] Create c-tutorial-pointer.cpp

---
+++ Hackerrank.com/C++/c-tutorial-pointer.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+void update(int *a, int *b) {
+    *a = *a + *b;
+    *b = abs(*a - 2 * *b);
+}
+
+int main() {
+    int a, b;
+    cin >> a >> b;
+    update(&a, &b);
+    cout << a << endl << b;
+    return 0;
+}
