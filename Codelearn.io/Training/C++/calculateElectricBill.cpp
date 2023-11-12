49d4cd559d625a7006e9d954b821e1339c7ebb7f
T3l3sc0p3 <96990857+T3l3sc0p3@users.noreply.github.com>
2023-11-12 12:09:19 +0700

    [codelearn.io] Create calculateElectricBill.cpp and calculateElectricBill.py

---
+++ Codelearn.io/Training/C++/calculateElectricBill.cpp
@@ -0,0 +1,11 @@
+int calculateElectricBill(int n)
+{
+    int money=1000;
+    if (n>50) {money+=230*50; n-=50;}
+    else return money+n*230;
+    if (n>50) {money+=480*50; n-=50;}
+    else return money+n*480;
+    if (n>49) {money+=700*49; n-=49;}
+    else return money+n*700;
+    return money+n*900;
+}
