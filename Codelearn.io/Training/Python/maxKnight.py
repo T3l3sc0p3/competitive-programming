4c6b86a95567c297564937354728ccae4f737a06
T3l3sc0p3 <96990857+T3l3sc0p3@users.noreply.github.com>
2023-11-14 17:04:39 +0700

    [codelearn.io] Update maxKnight.py
    Add one-line solution

--- Codelearn.io/Training/Python/maxKnight.py
+++ Codelearn.io/Training/Python/maxKnight.py
@@ -1,3 +1,5 @@
 def maxKnight(n):
-    if n&1: return n*n//2 + 1
+    if n&1: return n*n//2 + 1 # int(n*n/2 + 0.5)
     return n*(n//2) if n!=2 else 4
+# One-line solution
+# def maxKnight(n): return 4 if (n==2) else(n*n+1)//2
