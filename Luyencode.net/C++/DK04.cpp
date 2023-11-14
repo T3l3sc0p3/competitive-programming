09b9ce49532b048cb0b23658f2218b00832feeaf
T3l3sc0p3 <96990857+T3l3sc0p3@users.noreply.github.com>
2023-11-14 11:51:18 +0700

    [oj.luyencode.net] Create DK04.py and modify DK04.cpp

--- Luyencode.net/C++/DK04.cpp
+++ Luyencode.net/C++/DK04.cpp
@@ -2,8 +2,9 @@
 #include <cmath>
 using namespace std;
 int main() {
-	double n;
+	double n, p;
 	cin >> n;
-	cout << round(n) << endl;
+	p = (long long int)n;
+	cout << ((abs(abs(n)-abs(p))<0.5)?p:(n<0)?-(abs(p)+1):p+1) << endl;
 	return 0;
 }
