12a50bc1477a6500659502c328439c5e1004e378
T3l3sc0p3 <96990857+T3l3sc0p3@users.noreply.github.com>
2024-02-06 12:24:43 +0700

    [codeforces.com] Update 1661A-Array-Balancing.cpp

--- Codeforces.com/Problemset/C++/1661A-Array-Balancing.cpp
+++ Codeforces.com/Problemset/C++/1661A-Array-Balancing.cpp
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <vector>
 #define ll long long int
+#define loop(i,n) for (int i = 0; i < n; i++)
+#define fastio ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL)
 using namespace std;
 void swap(ll &a, ll &b) {
 	a = a ^ b;
@@ -10,37 +12,27 @@ void swap(ll &a, ll &b) {
 }
 void solveA() {
 	int n;
-	ll inp, sum = 0;
+	ll tmp, sum = 0;
 	cin >> n;
-	vector <ll>a;
-	for (int i = 0; i < n*2; i++)
+	vector <ll> a, b;
+	loop(i,n)
 	{
-		cin >> inp;
-		a.push_back(inp);
+		cin >> tmp;
+		a.push_back(tmp);
 	}
-	for (int i = 0; i < (n*2)-1; i++)
+	loop(i,n)
 	{
-		if (a[i] == a[n + i+1]) {
-			swap(a[i], a[n + i]);
-		}
+		cin >> tmp;
+		b.push_back(tmp);
 	}
-	for (int i = (n*2)-1; i > 1; i--)
-	{
-		if (i > n) {
-			sum += (a[i] - a[i-1]);
-		}
-		else {
-			sum += a[i-1] - a[i-2];
-		}
-	}
+	loop(i,n) { if (a[i] > b[i]) swap(a[i],b[i]); }
+	loop(i,n-1) sum += abs(a[i]-a[i+1]) + abs(b[i]-b[i+1]);
 	cout << sum << endl;
 }
 int main() {
-	ios_base::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+	fastio;
 	int t;
 	cin >> t;
-	while (t--) {
-		solveA();
-	}
+	while (t--) solveA();
 	return 0;
 }
